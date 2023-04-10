e9ec634471ae429ac60169f9cd22abc71fb20b33
= <=>
2023-04-10 16:14:50 +0900

    4/10 수업 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from "react";
-import Todo from './Todo';
 import './App.css';
+import Todo from './Todo';
+import {Paper, List} from "@material-ui/core";
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
-      item: {id: 0, title: "Hello World 1", done: true},
+      items: [
+        {id: 0, title: "Hello World 1", done: true},
+        {id: 1, title: "Hello World 2", done: false},
+        {id: 2, title: "Hello World 3", done: true}
+      ]
     };
 
   }
 
   render(){
+    var todoItems = this.state.items.length > 0 && (
+      <Paper style={{margin: 16}}>
+        <List>
+          {this.state.items.map((item, index) => (
+            <Todo item={item} key={item.id} />
+          ))}
+        </List>
+      </Paper>
+
+    );
+    /* this.state.items.map(
+      (item, index) => (
+        <Todo item={item}></Todo>  item이 하나씩 전달되는 과정임
+      )
+    */
+    
     return (
       <div className="App">
-        <Todo item={this.state.item} />
-        <Todo />
-      </div>
+        {/* <Todo item={this.state.items} />   <Todo> 컴포넌트 여러 개 */}
+        {todoItems}
+      </div> 
     );
   }
   
 }
+
 
 export default App;
